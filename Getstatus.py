ef00c7a396f36abbc40ecc12034046ba1e4650f6
Sheideh Homayon <shomayon@ucsc.edu>
2018-05-13 20:30:36 -0700

    Twitter_search polish

--- Getstatus.py
+++ Getstatus.py
@@ -12,9 +12,13 @@ api = twitter.Api(consumer_key='GwpuXi1ZMyc0ATSb3FEPaTyOU',
   access_token_secret='ID9RUnPxFmrMcOQNMNUwSy713CFLULNb21hKeZ57bLq4R')
 
 
-t = api.GetUserTimeline(screen_name="rosieatlarge", count=1000)
+p = ['StoneybrookVA']
 
-f = open('rosieatlarge.txt','w')
+for items in p:
+
+ t = api.GetUserTimeline(screen_name="p", count=1000)
+
+f = open('test.txt','w')
 tweets = [i.AsDict() for i in t]
 for t in tweets:
  #   print(t['id'], t['text'])
@@ -22,22 +26,22 @@ for t in tweets:
     f.write(t['text'])
     f.write('\n')
 f.close()
-c= []
-term = "@"
-with open('rosieatlarge.txt') as f:
-    for line in f:
-       str = line
-       h = line.strip()
-       z =h.split()
-       p = filter(lambda x: x[0] != '@', z)
-       q = " ".join(filter(lambda x: x[0] != '@', p))
-       c.append(q)
-print(c)
+#c= []
+#term = "@"
+#with open('rosieatlarge.txt') as f:
+#    for line in f:
+#       str = line
+#       h = line.strip()
+ #      z =h.split()
+ #      p = filter(lambda x: x[0] != '@', z)
+ #      q = " ".join(filter(lambda x: x[0] != '@', p))
+ #      c.append(q)
+#print(c)
 
-resultFyle = open("output1.csv",'w')
-for r in c:
-    resultFyle.write(r + "\n")
-resultFyle.close()
+#resultFyle = open("output1.csv",'w')
+#for r in c:
+#    resultFyle.write(r + "\n")
+#resultFyle.close()
 
 
 
