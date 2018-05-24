9516e74c885fba60cfe8f66e1d221fba9d1d08a4
Sheideh Homayon <shomayon@ucsc.edu>
2018-05-24 03:17:21 -0700

    lexicon update

---
+++ Lexicon/medication2.py
@@ -0,0 +1,37 @@
+import wikipedia
+import csv
+
+# Using Wiki API return list of mental health disorders
+p = wikipedia.page("List of antidepressants")
+import re
+
+print(p.url)
+print(p.title)
+
+# Getting Wikipedia section list of medication
+
+s = p.section('Tricyclic antidepressants (TCAs)')
+output = s.split('\n')
+output2 =[]
+del output[20] #akhar
+del output[19]
+del output[18]
+a=[]
+for line in output:
+    k= re.sub('[()]', '', line)
+    k1 = k.replace(',', '')
+    output2.append(k1)
+for words in output2:
+    t =words.split()
+
+    for i in range(len(t)):
+        a.append(t[i])
+
+with open("List of antidepressants3.csv",'w') as f:
+    writer = csv.writer(f, dialect='excel')
+    for m in a:
+        writer.writerow([m])
+
+
+
+
