ce12c151630c1b40cdfc436c3fd436cbb5398003
shomayon <shomayon@ucsc.edu>
2018-05-24 02:20:52 -0700

    List of antidepressants

--- Lexicon/medication_lexicon.py
+++ Lexicon/medication_lexicon.py
@@ -3,16 +3,30 @@ import csv
 
 # Using Wiki API return list of mental health disorders
 p = wikipedia.page("List of antidepressants")
+import re
 
 print(p.url)
 print(p.title)
 
-s = p.links
-l = len(s)
-print('List of antidepressants:',s)
+# Getting Wikipedia section list of medication
+
+s = p.section('Selective serotonin reuptake inhibitors (SSRIs)')
+output = s.split('\n')
+output2 =[]
+a=[]
+del output[6]
+for line in output:
+  k= re.sub('[()]', '', line)
+  k1 = k.replace(',', '')
+  output2.append(k1)
+for words in output2:
+    t =words.split()
+
+    for i in range(len(t)):
+       a.append(t[i])
 
 with open("List of antidepressants.csv",'w') as f:
     writer = csv.writer(f, dialect='excel')
-    for m in s:
+    for m in a:
         writer.writerow([m])
 
