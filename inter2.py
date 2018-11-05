ca2b5129780b34051b05bb4cb52ff05f8d7e0174
shomayon <shomayon@ucsc.edu>
2018-11-04 23:59:36 -0800

    Update inter2.py

--- inter2.py
+++ inter2.py
@@ -22,10 +22,10 @@ year=int(now.year)
 
 
 #FOR OAUTH AUTHENTICATION -- NEEDED TO ACCESS THE TWITTER API
-t = Twython(app_key='GwpuXi1ZMyc0ATSb3FEPaTyOU', #REPLACE 'APP_KEY' WITH YOUR APP KEY, ETC., IN THE NEXT 4 LINES
-            app_secret='0Y1jaPrDOa0uGsxQc4DSDphRWPPYCtVZ0TtvgZorAvzywIZtXJ',
-            oauth_token='220846580-ZUElx1lLAd5XRxrL9hYVG6CBkbjLUl3ftvCGIMqE',
-            oauth_token_secret='WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77')
+t = Twython(app_key='', #REPLACE 'APP_KEY' WITH YOUR APP KEY, ETC., IN THE NEXT 4 LINES
+            app_secret='',
+            oauth_token='',
+            oauth_token_secret='')
 
 #REPLACE WITH YOUR LIST OF TWITTER USER IDS
 ids = "220846580"
