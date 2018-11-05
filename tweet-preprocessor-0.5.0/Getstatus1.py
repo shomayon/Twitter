52b1d4f1449bd542f9c5a975085f9ff783f9565f
shomayon <shomayon@ucsc.edu>
2018-11-05 00:01:38 -0800

    Update Getstatus1.py

--- tweet-preprocessor-0.5.0/Getstatus1.py
+++ tweet-preprocessor-0.5.0/Getstatus1.py
@@ -4,10 +4,10 @@ import re
 import string
 
 # Consumer keys and access tokens, used for OAuth
-consumer_key = 'GwpuXi1ZMyc0ATSb3FEPaTyOU'
-consumer_secret = '0Y1jaPrDOa0uGsxQc4DSDphRWPPYCtVZ0TtvgZorAvzywIZtXJ'
-access_token = '220846580-ZUElx1lLAd5XRxrL9hYVG6CBkbjLUl3ftvCGIMqE'
-access_token_secret = 'WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
