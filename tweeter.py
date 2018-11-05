d220f92bec60cad5e2e46947abec5ebdfa5da72b
shomayon <shomayon@ucsc.edu>
2018-11-04 23:58:19 -0800

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -3,10 +3,10 @@ import csv
 import re
 
 # Consumer keys and access tokens, used for OAuth
-consumer_key = 'GwpuXi1ZMyc0ATSb3FEPaTyOU'
-consumer_secret = '0Y1jaPrDOa0uGsxQc4DSDphRWPPYCtVZ0TtvgZorAvzywIZtXJ'
-access_token = '220846580-ZUElx1lLAd5XRxrL9hYVG6CBkbjLUl3ftvCGIMqE'
-access_token_secret = 'WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -20,7 +20,7 @@ with open("test.csv",'w') as f:
 
 usernames = ['zangerdanger',
              'R2D1ck2',
-             'AIIStarBacon',
+             'AIIStarBacon',WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77
              'Kyashi_Cosplay',
              'Froymyjoy',
              'aloofloofah',
