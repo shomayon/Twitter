d2db582ceb660b8e9e0605f927327a4a85b0d6ec
shomayon <shomayon@ucsc.edu>
2018-11-05 00:00:30 -0800

    Update Getstatus.py

--- Getstatus.py
+++ Getstatus.py
@@ -6,10 +6,10 @@ import nltk
 
 
 import oauthlib
-api = twitter.Api(consumer_key='GwpuXi1ZMyc0ATSb3FEPaTyOU',
-  consumer_secret='0Y1jaPrDOa0uGsxQc4DSDphRWPPYCtVZ0TtvgZorAvzywIZtXJ',
-  access_token_key='220846580-ZUElx1lLAd5XRxrL9hYVG6CBkbjLUl3ftvCGIMqE',
-  access_token_secret='WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77')
+api = twitter.Api(consumer_key='',
+  consumer_secret='',
+  access_token_key='',
+  access_token_secret='')
 
 #
 # with open('../untitled5/username.csv') as f:
