8ce17ab89d4e54c7ee549475cc89915a784f9bbd
shomayon <shomayon@ucsc.edu>
2018-11-05 00:02:37 -0800

    Update Twitter_Search.py

--- TwitterSearch/Twitter_Search.py
+++ TwitterSearch/Twitter_Search.py
@@ -16,10 +16,10 @@ try:
 
     # create a TwitterSearch object with our secret tokens ()
     ts = TwitterSearch(
-        consumer_key = 'GwpuXi1ZMyc0ATSb3FEPaTyOU',
-        consumer_secret = '0Y1jaPrDOa0uGsxQc4DSDphRWPPYCtVZ0TtvgZorAvzywIZtXJ',
-        access_token = '220846580-ZUElx1lLAd5XRxrL9hYVG6CBkbjLUl3ftvCGIMqE',
-        access_token_secret = 'WEFkSeH59z92ptB76tGKnh8l6mMKmWN1fVKqV6dYCuc77'
+        consumer_key = '',
+        consumer_secret = '',
+        access_token = '',
+        access_token_secret = ''
      )
 
 
